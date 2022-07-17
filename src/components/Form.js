7b9a8cd695d365207525863b67f336d8d53bc1f2
chrisphine <onyangoketta@gmail.com>
2022-07-17 16:01:28 +0300

    onsubmit event listene added to form

--- src/components/Form.js
+++ src/components/Form.js
@@ -13,11 +13,12 @@ function Form(props) {
   }
 
   return (
-    <form>
+    <Form onSubmit={handleSubmit}>
+      
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
-    </form>
+    </Form>
   );
 }
 
